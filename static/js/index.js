f2a52ba44e40cede684a263bd3f3505623d20c3f
sasha <hello@gmail.com>
2024-11-17 14:23:46 +0200

    add to create-tourr

--- static/js/index.js
+++ static/js/index.js
@@ -11,7 +11,7 @@ $('#saveTour').click(function(){
         //                <!-- записуємо окрему змінну дів і викликаємо: -->
                 let tourDiv = document.getElementById('tours');
         //                <!--він дорівнює новому туру і вст назву в середину і додоємо код HTML щоб додався на початок -->
-                tourDiv.innerHTML = `<h3>${$('#tourName')}</h3>`+ tourDiv.innerHTML;
+                tourDiv.innerHTML = `<h3>${$('#tourName').val()}</h3>` + tourDiv.innerHTML;
                 $('#tourName').val('');//очистка форми
 
                 let tourModal = new bootstrap.Modal('#tourModal', options);
