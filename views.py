b148e5b2e59b2b7b80c62f0b97a761021a101438
sasha <hello@gmail.com>
2024-11-17 13:55:40 +0200

    add route create-tour

--- views.py
+++ views.py
@@ -9,3 +9,12 @@ import datetime
 def index(request: Request, db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
     # tours = db.query(Tour).all()'tours': tours,
     return templates.TemplateResponse('index.html', {'title': 'Tours', 'request': request})
+
+
+@app.route('/create-tour')
+def index(name: str = Form(), db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
+    tour = Tour(name=name)
+    db.add(tour)
+    db.commit()
+    db.refresh(tour)
+    return {}
