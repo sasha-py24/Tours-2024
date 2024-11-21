1cd4eb0833f261c4f34a00793e72e39e7ebc4d12
sasha <hello@gmail.com>
2024-11-21 18:28:49 +0200

    add content

--- static/js/create-tour.js
+++ static/js/create-tour.js
@@ -13,35 +13,18 @@ $('#saveTour').click(function(){
 
         'success': function(response){
                 var addTour = `
-                                    <td>${response.name}</td>
-                                    <td>${response.city}</td>
-                                    <td>${response.days}</td>
-                                    <td>${response.price}</td>
-                                    <td>${response.date}</td>
-                                    <td>
-                                        <p class="text-success">Done</p>
-                                    </td>`
+                                    <li>${response.name}</li>
+                                    <li>${response.city}</li>
+                                    <li>${response.days}</li>
+                                    <li>${response.price}</li>
+                                    <li>${response.date}</li>
+                                    `
 
-                document.getElementById(`tour${response.id}`).innerHTML = addTour
+                document.getElementById(`tourCard${response.id}`).innerHTML = addTour
 
         }
 
-
-
-
-
-
-//            'success': function(response){
-//
-//               let tours =  document.getElementById(`tourCard${response.tour_id}`);
-//               let newTour = `
-//                                <tr>
-//                                    <td>${$('#tourName').val()}</td>
-//                                    <td>${$('#tourCity').val()}</td>
-//                                    <td>${$('#tourDays').val()}</td>
-//                                    <td>${$('#tourPrice').val()}</td>
-//                                    <td>${$('#tourDate').val()}</td>
-//                                </tr>`
+//                                `
 //
 //            toursDiv.innerHTML = `<h3>${$('#tourName').val()}</h3>` + toursDiv.innerHTML;
 //            toursDiv.innerHTML = `<h3>${$('#tourCity').val()}</h3>` + toursDiv.innerHTML;
@@ -57,37 +40,3 @@ $('#saveTour').click(function(){
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        'success': function(response){
-//                let toursDiv = document.getElementById('tours');                                    //записуємо окрему змінну дів і викликаємо: -->
-//                toursDiv.innerHTML = `<h3>${$('#tourName').val()}</h3>` + toursDiv.innerHTML;       //він дорівнює новому туру і вст назву в середину і додоємо код HTML щоб додався на початок -->
-//                $('#tourName').val('');                                                             //очистка форми//
-//                let tourModal = new bootstrap.Modal('#tourModal');                                  //викликаємо hide і закриваємо вікно
-//                tourModal.hide();
-//                 }
-//     })
-//});
-
-//            toursDiv.innerHTML = `<h3>${$('#tourName').val()}</h3>` + toursDiv.innerHTML;
-//            toursDiv.innerHTML = `<h3>${$('#tourCity').val()}</h3>` + toursDiv.innerHTML;
-//            toursDiv.innerHTML = `<h3>${$('#tourDays').val()}</h3>` + toursDiv.innerHTML;
-//            toursDiv.innerHTML = `<h3>${$('#tourPrice').val()}</h3>`+ toursDiv.innerHTML;
-//            toursDiv.innerHTML = `<h3>${$('#tourDate').val()}</h3>` + toursDiv.innerHTML;
-//                $('#tourName').val(''),
-//                $('#tourCity').val(''),
-//                $('#tourDays').val(''),
-//                $('#tourPrice').val(''),
-//                $('#tourDate').val('')
