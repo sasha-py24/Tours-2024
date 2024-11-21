50692bbf5beff2b1a6eaa0bceb685576ca030c9e
sasha <hello@gmail.com>
2024-11-21 18:29:33 +0200

    add contente

--- static/js/register.js
+++ static/js/register.js
@@ -10,11 +10,12 @@ $('#saveUser').click(function(){
 
         'success': function(response){
                 let usersDiv = document.getElementById('users');                                    //записуємо окрему змінну дів і викликаємо: -->
-                usersDiv.innerHTML = `<h3>${$('#userName').val()}</h3>` + usersDiv.innerHTML;       //він дорівнює новому туру і вст назву в середину і додоємо код HTML щоб додався на початок -->
+                usersDiv.innerHTML = `<h3>${$('#userName').val()}</h3>` + usersDiv.innerHTML;
+                usersDiv.innerHTML = `<h3>${$('#userEmail').val()}</h3>` + usersDiv.innerHTML;      //він дорівнює новому user і вст назву в середину і додоємо код HTML щоб додався на початок -->                                                                  //він дорівнює новому туру і вст назву в середину і додоємо код HTML щоб додався на початок -->
                 $('#userName').val('')                                                              //очистка форми//
                 $('#userEmail').val('')
                 let userModal = new bootstrap.Modal('#userModal');                                  //викликаємо hide і закриваємо вікно
                 userModal.hide();
-                 }
+        }
      })
 });
