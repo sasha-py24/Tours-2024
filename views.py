160b6fe22a4c56ecb5846df15b155909a523ee2d
sasha <hello@gmail.com>
2024-11-23 23:12:34 +0200

    add content

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
+import datetime
+
 from config import app, templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi import Request, Depends, Form
+
 from sqlalchemy.orm import Session
+
 from db import get_db, User, Admin, Tour
-import datetime, _strptime
+
+
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request, db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
@@ -11,35 +16,32 @@ def index(request: Request, db: Session = Depends(get_db)):  # параметр 
     return templates.TemplateResponse('index.html', {'tours': tours, 'request': request})
 
 
+
 @app.post('/create-tour')
-def create_tour(name: str=Form(), city: str=Form(), days: int=Form(), price: int=Form(), date: str=Form(), db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
+def create_tour(name: str = Form(), city: str = Form(), days: int = Form(), price: int = Form(), date: str = Form(), db:Session = Depends(get_db)):  # параметр щоб дістати щось з бд
     date = datetime.datetime.strptime(date, '%Y-%m-%d', )
     tour = Tour(name=name, city=city, days=days, price=price, date=date)
     db.add(tour)
     db.commit()
     db.refresh(tour)
-    return {}
+    return {'id': 'tour.id'}
 
 
-# @app.post('/login')
-# async def login(request: Request, username: str = Form(), email: str =Form(), db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
-#     user = db.query(User).filter_by(username=username, email=email).first()
-#     if user is None or email != email:
-#         return RedirectResponse(url='/login', status_code=302)
-#     return templates.TemplateResponse('index.html', {'users': user, 'request': request})
 
-@app.get('/user', response_class=HTMLResponse)
-def user(request: Request, db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
-    users = db.query(User).all()
-    return templates.TemplateResponse('index.html', {'users': users, 'request': request})
+@app.post('/login')
+async def login(request: Request, username: str = Form(), email: str =Form(), db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
+    user = db.query(User).filter_by(username=username, email=email).first()
+    if user is None or email != email:
+        return RedirectResponse(url='/login', status_code=302)
+    return templates.TemplateResponse('index.html', {'users': user, 'request': request})
 
 
-@app.post('/add-user')
-def add_user(username=Form(), email=Form(), db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
+
+@app.get('/register', response_class=HTMLResponse)
+def register(request: Request, username=Form(), email=Form(),  db: Session = Depends(get_db)):  # параметр щоб дістати щось з бд
     user = User(username=username, email=email)
     db.add(user)
     db.commit()
     db.refresh(user)
-    return {}
-
+    return templates.TemplateResponse('register.html', {'user': user, 'request': request})
 
