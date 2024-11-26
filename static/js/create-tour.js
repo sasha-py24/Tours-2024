504a88d6ba78b91e2585eb4acac02138f23f3f74
sasha <hello@gmail.com>
2024-11-26 19:40:14 +0200

    add content

--- static/js/create-tour.js
+++ static/js/create-tour.js
@@ -12,47 +12,27 @@ $('#saveTour').click(function(){
                 },
 
         'success': function(response){
+            document.getElementById(`tours`).innerHTML +=
                 `
-                                    <h5>$('#tourName').val()</h5>
-                                    <li>$('#tourCity').val()</li>
-                                    <li>$('#tourDays').val()</li>
-                                    <li>$('#tourPrice').val()</li>
-                                    <li>$('#tourDate').val()</li>
-                              `
-                document.getElementById(`tours`).innerHTML +=
-
-                `
-                      <div class="card-body">
-                        <div class="card-title">
-                        </div>
-                        <div class="card-text" id="tourCard{{ t.id }}">
-                                    <ul id="tour{{ t.id }}">
-
-
-//                            <h5>{{ t.name }}</h5>
-//                            <li>{{ t.city }}</li>
-//                            <li>{{ t.days }}</li>
-//                            <li>{{ t.price }}$</li>
-//                            <li>{{ t.date }}</li>
-
-
-                                        <h5>$('#tourName').val()</h5>
-                                        <li>$('#tourCity').val()</li>
-                                        <li>$('#tourDays').val()</li>
-                                        <li>$('#tourPrice').val()</li>
-                                        <li>$('#tourDate').val()</li>
-
-
-                                    </ul>
-                        </div>
-                      </div>
-                `
-                +
-                $('#tourName').val(''),
-                $('#tourCity').val(''),
-                $('#tourDays').val(''),
-                $('#tourPrice').val(''),
-                $('#tourDate').val('')
+                <div class="card-body">
+                    <div class="card-title">
+                    </div>
+                    <div class="card-text" id="tourCard${response.id}">
+                        <ul id="tour${response.id}">
+                            <h5>${$('#tourName').val()}</h5>
+                            <li>${$('#tourCity').val()}</li>
+                            <li>${$('#tourDays').val()}</li>
+                            <li>${$('#tourPrice').val()}</li>
+                            <li>${$('#tourDate').val()}</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+            $('#tourName').val('');
+            $('#tourCity').val('');
+            $('#tourDays').val('');
+            $('#tourPrice').val('');
+            $('#tourDate').val('');
         }
     });
 });
