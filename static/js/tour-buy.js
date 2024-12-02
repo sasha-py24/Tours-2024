b34a74e2f72e85deae7b851772bf8e3e84718bd4
sasha <hello@gmail.com>
2024-12-02 13:31:31 +0200

    add content

--- static/js/tour-buy.js
+++ static/js/tour-buy.js
@@ -1,1 +1,22 @@
+$('#saveBuy').click(function(){
+    $.ajax('/buy-tour', {
+        'type': 'POST',
+        'async': true,
+        'dataType': 'json',
+        'data': {
+            'user_id': $('#user_id').val(),
+            'tour_id': $('#tour_id').val(),
+            'start_at': $('#start_at').val(),
+            'end_at': $('#end_at').val()
+                },
 
+        'success': function(response){
+            $('#tourCity').val('');
+            $('#tourDays').val('');
+            $('#tourPrice').val('');
+            $('#tourDate').val('');
+
+
+        }
+    });
+});
