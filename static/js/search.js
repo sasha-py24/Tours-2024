181502bf6a4c5350db6fafe0260df7da2e11ec17
sasha <hello@gmail.com>
2024-12-24 20:05:16 +0200

    add contnt

---
+++ static/js/search.js
@@ -0,0 +1,14 @@
+$('#idSearch').click(function(){
+    $.ajax('/search', {
+        'type': 'POST',
+        'async': true,
+        'dataType': 'json',
+        'data': {'search': $('#search').val()},
+        'success':
+        function(response){
+            document.getElementById(`tours`).innerHTML = response.tours;
+            if  = '':
+                tours.all();
+        }
+    });
+});
