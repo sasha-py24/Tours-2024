46281b2fcaeb3bab6d7b9222ee687e5c6be4a464
sasha <hello@gmail.com>
2025-01-09 16:17:42 +0200

    imporst

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
+# +
+
+
+from starlette.middleware.sessions import SessionMiddleware
 from fastapi import FastAPI
 
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+
 app = FastAPI()
 
+
+# об`єкт класу, де прописуємо шлях до теки, бо програма не розуміє
 templates = Jinja2Templates(directory="templates")
 app.mount('/static', StaticFiles(directory="static"), name="static")
 
-# обєкт класу, де прописуємо шлях до папки бо програма не розуміє
+
+app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
